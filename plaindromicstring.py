57d5fc590f593f7270ef2fa687b3072d422404e6
Susmit <susmit9370@gmail.com>
2016-03-05 14:51:24 +0530

    Added files via upload

---
+++ plaindromicstring.py
@@ -0,0 +1,14 @@
+for i in range(int(raw_input())):
+    x=raw_input()
+    y=raw_input()
+    z=0
+    for i in x:
+        if i in y:
+            z+=1
+    if z>=1:
+        print "Yes"
+    else:
+        print "No"
+         
+        
+    
